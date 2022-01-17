9cd1773bc9cf830ae4fedb6e3aa03fcf3960cfd1
dnr1492 <dnr1492@gmail.com>
2022-01-17 18:15:10 +0900

    스크립트 컴포넌트에 속성 추가

---
+++ LearnInspector/Assets/Scripts/LearnInspector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnInspector : MonoBehaviour
+{
+    public int damage;
+    public float hp;
+    public string myName;
+    public bool isDie;
+
+    public string[] arrNames;
+
+    public GameObject gameObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
